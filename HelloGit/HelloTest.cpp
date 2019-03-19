df47210dfac9c2c4db0b1ac19b7d6f58244786b3
phanbruc <phanbruc@USL0241.ppal.directory>
2019-03-19 09:48:59 -0500

    first update

--- HelloGit/HelloTest.cpp
+++ HelloGit/HelloTest.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "greetings.h"
+
+
 using namespace std;
 
 int main (int argc, char *argv[])
---
+++ HelloGit/greetings.h
@@ -0,0 +1,23 @@
+/*
+ * greetings.h
+ *
+ *  Created on: Mar 18, 2019
+ *      Author: phanbruc
+ */
+#pragma once
+
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class Greeting
+{
+public:
+    Greeting(){};
+    ~Greeting(){};
+private:
+    string _text;
+
+
+};
